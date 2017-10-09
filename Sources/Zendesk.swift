620c6e2ca923ff200b1f36ee5fcaebdcf96ab7c8
Adam Holt <omgitsads@github.com>
2017-10-09 14:36:37 +0200

    Use new API

--- Sources/Zendesk.swift
+++ Sources/Zendesk.swift
@@ -55,7 +55,7 @@ public class Zendesk {
                 observer.sendCompleted()
             }
             
-            disposable.add {
+            disposable.observeEnded {
                 request.cancel()
             }
         }
@@ -82,7 +82,7 @@ public class Zendesk {
                 observer.sendCompleted()
             }
             
-            disposable.add {
+            disposable.observeEnded {
                 request.cancel()
             }
         }
